9d86b1928db147c6cc43f0e828a7e3f62743fbd4
Aykhan Mammadov <amemmedov35@std.beu.edu.az>
2025-03-19 06:16:29 +0400

    Next.js yapılandırması güncellendi

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
   images: {
     unoptimized: true,
-  },
-  trailingSlash: true,
+  }
 }
 
 module.exports = nextConfig
